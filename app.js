3b7d9d94f2775885f5641b2859453f6465c5787c
Dhiraj Sahani <dhirajonline2k20@gmail.com>
2025-03-19 16:42:47 +0545

    Blog data delete Successfully.

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const Blog = require('./Model/blogModel')
 const app = express()
 
 app.use(express.json())
-const {multer,storage} = require('./middleware/multerConfig')
+const { multer, storage } = require('./middleware/multerConfig')
 
-const upload = multer({storage:storage})
+const upload = multer({ storage: storage })
 
 connectToDatabase()
 
@@ -18,8 +18,8 @@ app.get("/", (req, res) => {
     })
 })
 
-app.post("/blog", upload.single('image'), async (req, res) => { 
-    const { title, subtitle, description} = req.body
+app.post("/blog", upload.single('image'), async (req, res) => {
+    const { title, subtitle, description } = req.body
     const filename = req.file.filename
 
     //check condition if data not send error msg 400 show 
@@ -41,18 +41,40 @@ app.post("/blog", upload.single('image'), async (req, res) => {
     })
 })
 
-app.get("/blog", async (req,res)=>{
-    const blogs = await Blog.find()
-    res.status(200).json ({
-        message:"Blogs fetched Sucessfully",
+app.get("/blog", async (req, res) => {
+    const blogs = await Blog.find() // return array
+    res.status(200).json({
+        message: "Blogs fetched Sucessfully",
         data: blogs
     })
 })
 
-app.use(express.static('./storage'))
+app.get("/blog/:id", async (req,res) => {
+    const id = req.params.id
+    const blog = await Blog.findById(id) // return obj
 
+    if (!blog) {
+        return res.status(404).json({
+            message: "data no found"
+        })
+    }
+    res.status(200).json({
+        message: "fetched successfully",
+        data: blog
 
-
-app.listen(process.env.PORT, () => {
-    console.log("NodeJs projct has started")
+    })
 })
+
+app.delete("/blog/:id",async(req,res)=>{
+    const id = req.params.id
+   await Blog.findByIdAndDelete(id) // delete data
+    res.status(200).json({
+        message:"delete data Successfully.."
+    })
+})
+
+    app.use(express.static('./storage'))
+
+    app.listen(process.env.PORT, () => {
+        console.log("NodeJs projct has started")
+    })
